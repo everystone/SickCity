be4b743c32f2ee76d05770675e5a9b5b71b9c822
Eirik Kvarstein <eirik.kvarstein@gmail.com>
2016-05-18 21:14:30 +0200

    first commit

---
+++ SickCity/Game.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "Game.h"
+#include "GameState.h"
+
+void Game::pushState(GameState* state)
+{
+	this->states.push(state);
+}
+void Game::popState()
+{
+	delete this->states.top();
+	this->states.pop();
+}
+void Game::changeState(GameState* state)
+{
+	if (!this->states.empty()) {
+		popState();
+	}
+	pushState(state);	
+}
+GameState* Game::peekState() {
+	if (this->states.empty()) {
+		return nullptr;
+	}
+	return this->states.top();
+}
+void Game::gameLoop()
+{
+	sf::Clock clock;
+	while (this->window.isOpen())
+	{
+		sf::Time elapsed = clock.restart();
+		float dt = elapsed.asSeconds();
+		if (peekState() == nullptr) continue;
+		peekState()->handleInput();
+		peekState()->update(dt);
+		this->window.clear(sf::Color::Black);
+		peekState()->draw(dt);
+		this->window.display();
+	}
+}
+Game::Game()
+{
+	// Constructor
+	this->window.create(sf::VideoMode(800, 600), "Sick City");
+	this->window.setFramerateLimit(60);
+}
+
+Game::~Game()
+{
+	// Destructor
+	while (!this->states.empty()) {
+		popState();
+	}
+}
---
+++ SickCity/SickCity.cpp
@@ -0,0 +1,13 @@
+// SickCity.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Game.h"
+
+int main()
+{
+	Game game;
+	game.gameLoop();	
+    return 0;
+}
+
