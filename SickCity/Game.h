19a9ca8ba56dc8761ef3f1bda556d846b2f6d754
Eirik Kvarstein <eirik.kvarstein@gmail.com>
2016-05-18 22:59:44 +0200

    Part 4 done, Tiles!

--- SickCity/Game.h
+++ SickCity/Game.h
@@ -2,18 +2,25 @@
 #include <stack>
 #include <SFML/Graphics.hpp>
 #include "TextureManager.h"
+#include "Tile.h"
+#include <map>
+#include <string>
+
 class GameState;
 
 class Game
 {
 private:
 	void loadTextures();
+	void loadTiles();
+
 public:
+	const static int tileSize = 8;
 	std::stack<GameState*> states;
 	TextureManager texmgr;
 	sf::RenderWindow window;
 	sf::Sprite background;
-
+	std::map<std::string, Tile> tileAtlas;
 	void pushState(GameState* state);
 	void popState();
 	void changeState(GameState* state);
---
+++ SickCity/Tile.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h"
+#include "Tile.h"
+#include "AnimationHandler.h"
+
+void Tile::draw(sf::RenderWindow & window, float dt)
+{
+	// change sprite to reflect tile variant
+	this->animHandler.changeAnim(this->tileVariant);
+
+	// update animation
+	this->animHandler.update(dt);
+
+	// update sprite
+	this->sprite.setTextureRect(this->animHandler.bounds);
+
+	// draw tile
+	window.draw(this->sprite);
+	return;
+}
+
+void Tile::update()
+{
+	/* If the population is at the maximum value for the tile,
+	* there is a small chance that the tile will increase its
+	* building stage */
+	if ((this->tileType == TileType::RESIDENTIAL ||
+		this->tileType == TileType::COMMERCIAL ||
+		this->tileType == TileType::INDUSTRIAL) &&
+		this->population == this->maxPopPerLevel * (this->tileVariant + 1) &&
+		this->tileVariant < this->maxLevels)
+	{
+		if (rand() % int(1e4) < 1e2 / (this->tileVariant + 1))++this->tileVariant;
+	}
+
+	return;
+}
+
+std::string tileTypeToStr(TileType type)
+{
+	switch (type)
+	{
+	default:
+	case TileType::VOID:            return "Void";
+	case TileType::GRASS:           return "Flatten";
+	case TileType::FOREST:          return "Forest";
+	case TileType::WATER:           return "Water";
+	case TileType::RESIDENTIAL:     return "Residential Zone";
+	case TileType::COMMERCIAL:      return "Commercial Zone";
+	case TileType::INDUSTRIAL:      return "Industrial Zone";
+	}
+}
+
